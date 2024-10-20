69270de4819093eaa98b4342a9688bfe9696ad1f
SushaanthSrinivasan <iamsushaanth@gmail.com>
2024-10-19 23:31:36 -0700

    Logic to send audio file to api endpoint

---
+++ audio_file_sender.py
@@ -0,0 +1,13 @@
+import requests
+
+url = ''
+
+file_path = 'sample.mp3'
+with open(file_path, 'rb') as file:
+    files = {'file': (file_path, file, 'audio/mpeg')}
+    response = requests.post(url, files=files)
+
+if response.status_code == 200:
+    print('File uploaded successfully')
+else:
+    print(f'Failed to upload file. Status code: {response.status_code}, Response: {response.text}')
