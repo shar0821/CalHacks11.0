be29eccc424c6625ebff1487b91057c24aa7a38d
AmbarishGK <ambarishgk@gmail.com>
2024-10-20 03:39:02 -0700

    Updated

--- DeepgramAdapter.py
+++ DeepgramAdapter.py
@@ -1,24 +1,38 @@
+# Copyright 2023-2024 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
+
 import os
 from dotenv import load_dotenv
+import logging
+from deepgram.utils import verboselogs
+from datetime import datetime
+import httpx
 
 from deepgram import (
     DeepgramClient,
+    DeepgramClientOptions,
     PrerecordedOptions,
     FileSource,
 )
 
 load_dotenv()
 
-# Path to the audio file
 AUDIO_FILE = "test.mp3"
-
 API_KEY = os.getenv("DG_API_KEY")
+
 
 def main():
     try:
-        # STEP 1 Create a Deepgram client using the API key
-        deepgram = DeepgramClient(API_KEY)
+        # STEP 1 Create a Deepgram client using the API key in the environment variables
+        config: DeepgramClientOptions = DeepgramClientOptions(
+            verbose=verboselogs.SPAM,
+        )
+        deepgram: DeepgramClient = DeepgramClient(API_KEY, config)
+        # OR use defaults
+        # deepgram: DeepgramClient = DeepgramClient()
 
+        # STEP 2 Call the transcribe_file method on the rest class
         with open(AUDIO_FILE, "rb") as file:
             buffer_data = file.read()
 
@@ -26,25 +40,28 @@ def main():
             "buffer": buffer_data,
         }
 
-        #STEP 2: Configure Deepgram options for audio analysis
-        options = PrerecordedOptions(
+        options: PrerecordedOptions = PrerecordedOptions(
             model="nova-2",
             smart_format=True,
-            diarize=True
+            utterances=True,
+            punctuate=True,
+            diarize=True,
         )
 
-        # STEP 3: Call the transcribe_file method with the text payload and options
-        response = deepgram.listen.prerecorded.v("1").transcribe_file(payload, options)
+        before = datetime.now()
+        response = deepgram.listen.rest.v("1").transcribe_file(
+            payload, options, timeout=httpx.Timeout(300.0, connect=10.0)
+        )
+        after = datetime.now()
 
-        # STEP 4: Print the response
         print(response.to_json(indent=4))
-        return response.to_json()
+        print("")
+        difference = after - before
+        print(f"time: {difference.seconds}")
 
     except Exception as e:
         print(f"Exception: {e}")
 
 
 if __name__ == "__main__":
-    output = main()
-
-
+    main()
